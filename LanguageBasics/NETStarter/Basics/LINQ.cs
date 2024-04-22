fa35c6d3af53b9f88b79d770b937028d1f8d2be0
Bishnu Rawal <rawal.bishnu@gmail.com>
2024-04-22 14:44:51 +0545

    Configure ef core for sqlite db

--- LanguageBasics/NETStarter/Basics/LINQ.cs
+++ LanguageBasics/NETStarter/Basics/LINQ.cs
@@ -9,11 +9,11 @@ class LINQ
 {
     int[] ages = [23, 45, 67, 8, 9, 12, 345, 67, 889, 34, 56, 78];
     List<Student> students = [
-        new Student() { name = "Ram", address = "Ktm"},
-        new Student() { name = "Laxman", address = "Dang"},
-        new Student() { name = "Bikendra", address = "Ktm"},
-        new Student() { name = "Sandip", address = "Dang"},
-        new Student() { name = "Chahana", address = "Dhangadhi"},
+        new Student() { Name = "Ram", address = "Ktm"},
+        new Student() { Name = "Laxman", address = "Dang"},
+        new Student() { Name = "Bikendra", address = "Ktm"},
+        new Student() { Name = "Sandip", address = "Dang"},
+        new Student() { Name = "Chahana", address = "Dhangadhi"},
     ];
 
     // Find squares of all numbers in ages collection
